3d21131fb2067f41e77096141f17b3b471e4f11e
Shiyun Lu <lushiyun@outlook.com>
2020-05-06 01:51:31 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+  
+  def self.count
+    @@count
+  end 
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    song_counts = Hash.new(0)
+    @@genres.each do |genre|
+      song_counts[genre] += 1 
+    end
+    song_counts
+  end
+  
+  def self.artist_count
+    song_counts = Hash.new(0)
+    @@artists.each do |artist|
+      song_counts[artist] += 1 
+    end 
+    song_counts
+  end 
+end
