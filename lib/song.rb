c297e18c4d898f85a587fffd11373fccff46153a
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-11 19:31:22 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @@artists << @artist = artist
+    @@genres  << @genre = genre
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      genre_count.length << @@genre
+    end
+  end
+end
